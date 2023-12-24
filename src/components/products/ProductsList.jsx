71926fdef905a4b8396d584ccbedcee93a5e377f
Mahsa Jafari <mahsa@Mahsa-MacBook-Pro.local>
2023-12-24 16:02:11 +0330

    handle sort action

--- src/components/products/ProductsList.jsx
+++ src/components/products/ProductsList.jsx
@@ -8,6 +8,8 @@ const ProductsList = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [filteredProducts, setFilteredProducts] = useState([]);
+  const [sortOrder, setSortOrder] = useState("");
+  const [isSortingEnabled, setIsSortingEnabled] = useState(true);
 
   useEffect(() => {
     setLoading(true);
@@ -17,6 +19,27 @@ const ProductsList = () => {
       setLoading(false);
     });
   }, []);
+  const handleSortByPrice = () => {
+    let sortedProducts = [...filteredProducts];
+
+    sortedProducts.sort((a, b) => a.price - b.price);
+
+    // Toggle sorting order
+    if (sortOrder === "desc") {
+      sortedProducts.reverse();
+      setSortOrder("asc");
+    } else {
+      setSortOrder("desc");
+    }
+
+    setFilteredProducts(sortedProducts);
+    setIsSortingEnabled(false); // Disable sorting after sorting action
+  };
+  const resetSorting = () => {
+    setIsSortingEnabled(true);
+    setFilteredProducts(products); // Reset the product list to the original state
+    // Reset other sorting-related state if needed
+  };
  
  
 
@@ -39,7 +62,22 @@ const ProductsList = () => {
   return (
     <div className="container">
       <ProductFilter onFilterChange={handleFilterChange} />
-      <div className="row mb-3"> 
+      <div className="row mb-3">
+        <div className="col-md-12 d-flex justify-content-center">
+          <button
+            onClick={handleSortByPrice}
+            disabled={!isSortingEnabled}
+            className="btn btn-primary px-1 py-2 m-1"
+          >
+            Sort by Price {sortOrder === "asc" ? "↓" : "↑"}
+          </button>
+          <button
+            onClick={resetSorting}
+            className="btn btn-secondary px-1 py-2 m-1"
+          >
+            Reset Sorting
+          </button>
+        </div>
       </div>
       <div className="row">
         {filteredProducts.map((item, index) => (
